8d29517746d3ebd6cee9de0e4542c66a6d8488e1
Anar <65445762+anar2004@users.noreply.github.com>
2020-05-29 13:40:21 +0400

    try

---
+++ js/news.js
@@ -0,0 +1,89 @@
+$(".zoomdiv1").click(function(){
+  $(".abimg1").fadeOut();
+  $(".zoomdiv1").fadeOut();
+  $(".zoom1").fadeOut();
+  $(".ittem10").last().removeClass("active");
+  $(".ittem9").last().removeClass("active");
+  $(".ittem8").last().removeClass("active");
+  $(".ittem7").last().removeClass("active");
+  $(".ittem6").last().removeClass("active");
+  $(".ittem5").last().removeClass("active");
+  $(".ittem4").last().removeClass("active");
+  $(".ittem3").last().removeClass("active");
+  $(".ittem2").last().removeClass("active");
+  $(".ittem1").last().removeClass("active");
+  $(".carousel-item").removeClass("active");
+});
+
+$(".fa-times").click(function(){
+  $(".abimg1").fadeOut();
+  $(".zoomdiv1").fadeOut();
+  $(".zoom1").fadeOut();
+  $(".ittem10").last().removeClass("active");
+  $(".ittem9").last().removeClass("active");
+  $(".ittem8").last().removeClass("active");
+  $(".ittem7").last().removeClass("active");
+  $(".ittem6").last().removeClass("active");
+  $(".ittem5").last().removeClass("active");
+  $(".ittem4").last().removeClass("active");
+  $(".ittem3").last().removeClass("active");
+  $(".ittem2").last().removeClass("active");
+  $(".ittem1").last().removeClass("active");
+  $(".carousel-item").removeClass("active");
+});
+
+$(".aboutpic1").click(function(){
+  $(".abimg1").fadeIn();
+  $(".zoomdiv1").fadeIn();
+  $(".zoom1").fadeIn();
+  $(".eheh22").fadeOut();
+});
+
+$(".ab2textdiv2").click(function(){
+  $(".zoomdiv1").fadeIn();
+  $(".zoom1").fadeIn();
+  $(".eheh22").fadeIn();
+  $(".iem5").last().addClass("active");
+  $(".ittem5").last().addClass("active");
+});
+
+$(".abpic4").click(function(){
+  $(".zoomdiv1").fadeIn();
+  $(".zoom1").fadeIn();
+  $(".eheh22").fadeIn();
+  $(".iem4").last().addClass("active");
+  $(".ittem4").last().addClass("active");
+});
+
+$(".abpic3").click(function(){
+  $(".zoomdiv1").fadeIn();
+  $(".zoom1").fadeIn();
+  $(".eheh22").fadeIn();
+  $(".iem3").last().addClass("active");
+  $(".ittem3").last().addClass("active");
+});
+
+$(".abpic2").click(function(){
+  $(".zoomdiv1").fadeIn();
+  $(".zoom1").fadeIn();
+  $(".eheh22").fadeIn();
+  $(".iem2").last().addClass("active");
+  $(".ittem2").last().addClass("active");
+});
+
+$(".abpic1").click(function(){
+  $(".zoomdiv1").fadeIn();
+  $(".zoom1").fadeIn();
+  $(".eheh22").fadeIn();
+  $(".iem1").last().addClass("active");
+  $(".ittem1").last().addClass("active");
+});
+
+
+$(".ab2textdiv1").click(function(){
+  $(".zoomdiv1").fadeIn();
+  $(".zoom1").fadeIn();
+  $(".eheh22").fadeIn();
+  $(".iem1").last().addClass("active");
+  $(".ittem1").last().addClass("active");
+});
