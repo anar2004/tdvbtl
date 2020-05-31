10fd9b9fa91a146868a2a23592743be232b48238
Anar <65445762+anar2004@users.noreply.github.com>
2020-05-31 22:22:45 +0400

    elaqe

--- js/main.js
+++ js/main.js
@@ -1,36 +1,10 @@
-$(document).ready(function(){
-  // Add scrollspy to <body>
-  $("#qalx").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+function validateForm(element, element1, element2) {
+  var x = document.forms["contact"][element].value;
 
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    }  // End if
-  });
-
-  $('#form input[type="text"]').blur(function(){
-        if(!$(this).val()){
-            $(this).alert("ahah");
-        } else{
-            $(this).alert("error");
-        }
-    });
-
-
-  });
-
-
-});
+  if (x == "" || x == null) {
+    document.getElementById(element1).style.backgroundColor = "#f5abb5";
+    setTimeout(function(){
+      alert(element2 + " sahəsi doldurulmalıdır.");
+    }, 100);
+    return false;
+  }}
